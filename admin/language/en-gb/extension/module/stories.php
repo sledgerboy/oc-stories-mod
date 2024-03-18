3bc0dd0972dce6d16234b49dd93976f2f7db464b
nekrasov <nekrasovdy@belhard.com>
2024-03-19 01:50:14 +0300

    Startwork

---
+++ admin/language/en-gb/extension/module/stories.php
@@ -0,0 +1,32 @@
+<?php
+
+// Heading
+$_['heading_title']    = 'Stories';
+
+// Text
+$_['text_extension']   = 'Extensions';
+$_['text_success']     = 'Success: You have modified stories module!';
+$_['text_edit']        = 'Edit Stories Module';
+
+// Entries / List
+$_['stories_lists']    = 'All stories';
+
+// Entry
+$_['entry_name']       = 'Story name';
+$_['entry_type']       = 'Story type';
+$_['entry_banner']     = 'Banner placing';
+$_['entry_preview']    = 'Preview image';
+$_['entry_image']      = 'Story image';
+$_['entry_btn_link']   = 'Button link';
+$_['entry_btn_txt']    = 'Button text';
+$_['entry_max_qty']    = 'Max stories quantity';
+$_['entry_lifetime']   = 'Days availability for Story';
+$_['entry_status']     = 'Status';
+$_['entry_width']      = 'Width';
+$_['entry_height']     = 'Height';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify stories module!';
+$_['error_name']       = 'Module Name must be between 3 and 64 characters!';
+$_['error_width']      = 'Width required!';
+$_['error_height']     = 'Height required!';
